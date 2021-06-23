e9ad8100b70d91bbde194f5919c914f909e66021
Brandon Leyva <bgleyva3@gmail.com>
2021-06-23 02:08:59 -0500

    styling chat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './components/application.css'
 import {HashRouter as Router, Switch, Route} from 'react-router-dom'
 import Login from './components/Login';
 import Application from './components/Application';
